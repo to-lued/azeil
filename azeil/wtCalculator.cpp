d495edb8d0c7f6f602e64987fd0c610154952f3e
Torben Lüdeking <to.lued@gmail.com>
2018-04-02 09:40:52 +0200

    Added totalOf function and modified worktimeToday. Testing needed for totalOf

--- azeil/wtCalculator.cpp
+++ azeil/wtCalculator.cpp
@@ -1,30 +1,112 @@
 #include "wtCalculator.h"
-#include <ctime>
 #include <time.h>
+#include <ctime>
+#include <iostream>
 
-void WTCalculator::worktimeToday(){
+std::chrono::duration<long, std::ratio<1, 1>> WTCalculator::worktimeToday() {
+	auto wtime = totalOf(std::chrono::system_clock::now(), WTWorkTime);
+	auto otime = totalOf(std::chrono::system_clock::now(), WTOffTime);
+	std::cout << "Worktime: " << wtime.count() << std::endl;
+	std::cout << "Offtime: " << otime.count() << std::endl;
+	return wtime;
 }
-WTCalculator::WTRange WTCalculator::getRange(tp anchor, RANGEDEF type){
+std::chrono::duration<long, std::ratio<1, 1>> WTCalculator::totalOf(
+    tp anchor, WTCalcType type) {
+	auto range = getRange(std::chrono::system_clock::now(), RANGE_DAY);
+	auto entrys = database_->readStampEntrys(range.beg, range.end);
+	std::chrono::seconds cumulated_worktime;
+	std::chrono::seconds cumulated_offtime;
 
-	auto today = std::chrono::system_clock::to_time_t(anchor);
-	auto start = std::localtime(&today);
-	auto end = std::localtime(&today);
+	enum StampingState { none, working, stopped };
 
-	start->tm_sec = 0;
-	start->tm_min = 0;
-	start->tm_hour = 0;
-	if(type == RANGE_MONTH)
-		start->tm_mday = 0;
+	StampingState current_state = none;
+	tp previous;
 
-	end->tm_sec = 59;
-	end->tm_min = 59;
-	end->tm_hour = 23;
-	if(type == RANGE_MONTH)
-		end->tm_mday = 0;
+	for (auto item : entrys) {
+		if (current_state == none || current_state == stopped) {
+			if (item.type == STAMP_STOP) {
+				throw InconsistencyException();
+			}
+			if (current_state == stopped) {
+				cumulated_offtime += std::chrono::duration_cast<
+				    std::chrono::seconds>(item.timestamp -
+							  previous);
+			}
+			current_state = working;
+			previous = item.timestamp;
+		} else {
+			if (item.type == STAMP_START) {
+				throw InconsistencyException();
+			}
+			cumulated_worktime +=
+			    std::chrono::duration_cast<std::chrono::seconds>(
+				item.timestamp - previous);
+			current_state = stopped;
+			previous = item.timestamp;
+		}
+	}
+	if (current_state == working) throw InconsistencyException();
+
+	if (type == WTOffTime)
+		return cumulated_offtime;
+	else if (type == WTWorkTime)
+		return cumulated_worktime;
+}
+WTCalculator::WTRange WTCalculator::getRange(tp anchor, RANGEDEF type) {
+	time_t time = std::chrono::system_clock::to_time_t(anchor);
+	return getRange(time, type);
+}
+WTCalculator::WTRange WTCalculator::getRange(time_t anchor, RANGEDEF type) {
+	auto start = *std::localtime(&anchor);
+	auto end = *std::localtime(&anchor);
+
+	start.tm_sec = 0;
+	start.tm_min = 0;
+	start.tm_hour = 0;
+	if (type == RANGE_MONTH) start.tm_mday = 1;
+
+	end.tm_sec = 59;
+	end.tm_min = 59;
+	end.tm_hour = 23;
+	if (type == RANGE_MONTH) end.tm_mday = getDaysInMonth(anchor);
 
 	WTRange range;
-	range.beg = std::chrono::system_clock::from_time_t(mktime(start));
-	range.end = std::chrono::system_clock::from_time_t(mktime(end));
+	range.beg = std::chrono::system_clock::from_time_t(mktime(&start));
+	range.end = std::chrono::system_clock::from_time_t(mktime(&end));
 
 	return range;
+}
+int WTCalculator::getDaysInMonth(time_t anchor) {
+	auto data = *std::localtime(&anchor);
+	auto year = 1900 + data.tm_year;
+	bool isleapyear = false;
+
+	if (year % 4 == 0) {
+		if (year % 100 == 0) {
+			if (year % 400 == 0)
+				isleapyear = true;
+			else
+				isleapyear = false;
+		} else
+			isleapyear = true;
+	}
+
+	switch (data.tm_mon) {
+		case 2:
+			return isleapyear ? 29 : 28;
+		case 0:
+		case 4:
+		case 6:
+		case 7:
+		case 9:
+		case 11:
+			return 31;
+		case 1:
+		case 3:
+		case 5:
+		case 8:
+		case 10:
+			return 30;
+	}
+	return 31;
 }
--- azeil/wtCalculator.h
+++ azeil/wtCalculator.h
@@ -1,23 +1,35 @@
 #ifndef WTIMECALCULATOR
 #define WTIMECALCULATOR
-#include "stampdb.h"
 #include <chrono>
-class WTCalculator{
-	public:
-		typedef struct WTRange {
-			tp beg;
-			tp end;
-		} WTRange;
-		WTCalculator(StampDB *database) : database_(database){}
-		~WTCalculator(){}
-		
-		void worktimeToday();
+#include <exception>
+#include "stampdb.h"
+class InconsistencyException : public std::exception {
+	const char *what() const throw() {
+		return "inconsistency in workday detected!";
+	}
+};
+class WTCalculator {
+       public:
+	typedef struct WTRange {
+		tp beg;
+		tp end;
+	} WTRange;
+	typedef enum {
+		WTWorkTime,
+		WTOffTime
+	} WTCalcType;
+	WTCalculator(StampDB *database) : database_(database) {}
+	~WTCalculator() {}
 
-	private:
-		typedef enum { RANGE_DAY, RANGE_MONTH } RANGEDEF;
-		StampDB *database_;
+	std::chrono::duration<long, std::ratio<1, 1>> worktimeToday();
+	std::chrono::duration<long, std::ratio<1, 1>> totalOf(tp anchor, WTCalcType type);
 
-		WTRange getRange(tp anchor, RANGEDEF);
-		
+       private:
+	typedef enum { RANGE_DAY, RANGE_MONTH } RANGEDEF;
+	StampDB *database_;
+
+	WTRange getRange(tp anchor, RANGEDEF);
+	WTRange getRange(time_t anchor, RANGEDEF);
+	int getDaysInMonth(time_t anchor);
 };
 #endif
